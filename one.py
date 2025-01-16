83fc0501e03c1cf1e7f8de0c029a00e1ae561a5e
Asimkaml <qaziasim414@gmail.com>
2025-01-16 22:57:22 +0500

    Initial commit

---
+++ one.py
@@ -0,0 +1,15 @@
+fruits = ['orange', 'mango', 'banana', 'apple']
+# for i in range(1,3):
+# for i in range(4):
+# for i in fruits:
+#     print(i, ' ' ,fruits[i], '\n')
+# def factorial(n):
+#     if n==0 or n==1:
+#         return 1
+#     return n * factorial(n-1)
+# print(factorial(0))
+# print(factorial(1))
+# print(factorial(3))
+# print(factorial(5))
+for i in range(1, 30, 3):
+    print(i)
